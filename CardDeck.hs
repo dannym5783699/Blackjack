2ec834a9980786d3f9e277746cf95535bd25eb6e
Key Vollers <keyvollers@unm.edu>
2024-04-15 12:44:44 -0600

    Adding rank data class

--- CardDeck.hs
+++ CardDeck.hs
@@ -8,6 +8,24 @@ instance Show Suit where
   show Club    = "\x2660"
 
 data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
+  deriving(Eq, Ord)
+
+instance Show Rank where
+  show Two   = "2"
+  show Three = "3"
+  show Four  = "4"
+  show Five  = "5"
+  show Six   = "6"
+  show Seven = "7"
+  show Eight = "8"
+  show Nine  = "9"
+  show Ten   = "10"
+  show Jack  = "J"
+  show Queen = "Q"
+  show King  = "K"
 
 data Card a b = Card Suit Rank
+  deriving(Eq)
+
+
 
