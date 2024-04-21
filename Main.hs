f2d4248d9d16d80273ee87d0bf9d27482a69bc60
KeyVollers <keyvollers@unm.edu>
2024-04-21 13:54:30 -0600

    Adding a new page to hold the code for a regular blackjack game

--- Main.hs
+++ Main.hs
@@ -1,26 +1,4 @@
-import           CardDeck
+import           RegBlackJackGame
 main :: IO ()
 main = do
   startGameLoop
-
-startGameLoop :: IO ()
-startGameLoop = do
-  let fullDeck = createDeck
-  let rmDeck = Deck ([])
-  gameLoop fullDeck rmDeck
-
-gameLoop :: Deck -> Deck -> IO ()
-gameLoop playDeck rmDeck
-  | hasRemaingingCards playDeck 2 = do
-      let hand = takeXCards playDeck 2
-      let newRMDeck = addToRemovedDeck hand rmDeck
-      let newPlayDeck =  removeCard playDeck 2
-      print (hand)
-      print "Would you like another card? ('Y' or 'N')"
-      char <- getChar
-      if char == 'Y' || char == 'y'
-      then do
-        char <- getChar
-        gameLoop newPlayDeck newRMDeck
-      else print "Thank you for Playing!"
-  | otherwise = print "There are no cards left in the deck, Thank you for Playing!"
