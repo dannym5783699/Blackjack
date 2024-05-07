3dcef58b6bb4a956d31b773e73a80efea4099f23
Alex Bernal <alexbernal@Alexs-MacBook-Pro.local>
2024-05-06 22:09:14 -0600

    Track of wins work for MAC Need to do WIND

---
+++ src/PlayerData.hs
@@ -0,0 +1,22 @@
+module PlayerData
+  ( Player(..)
+  , createPlayer
+  , addWin
+  ) where
+
+-- Definition of the Player data type
+data Player = Player {
+    playerName :: String,  -- Stores the player's name
+    wins       :: Int      -- Stores the number of wins
+} deriving (Show)
+
+-- Function to create a new player with the provided name and 0 wins
+createPlayer :: IO Player
+createPlayer = do
+    putStrLn "Enter your name:"
+    name <- getLine
+    return $ Player name 0  -- Initializes the player with 0 wins
+
+-- Function to increment the win count of a player
+addWin :: Player -> Player
+addWin player = player { wins = wins player + 1 }
